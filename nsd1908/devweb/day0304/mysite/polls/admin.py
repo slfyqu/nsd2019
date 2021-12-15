9db6286e630acd81d2a9f1a6504ed98164f6ed71
roof <89643871+slfyqu@users.noreply.github.com>
2021-12-15 10:22:46 +0800

    Delete nsd1908 directory

--- nsd1908/devweb/day0304/mysite/polls/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Question, Choice  # 从当前目录下的models中导入模型
-
-# Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
